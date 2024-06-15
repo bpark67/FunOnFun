5cfce4a07435ffe1a7ee8f4d819e279bb18ec9df
bpark67 <bpark67@uw.edu>
2024-06-15 12:12:31 -0700

    added popluation orthogonalization functions

--- R/simMFPCA.R
+++ R/simMFPCA.R
@@ -118,13 +118,46 @@ simMFPCA = function(seed, t, n, components, mean_funs, eigen_funs_list, lambdas,
   }
 
   return(
-    list(
+    structure(list(
       X = X,
       mu = mu,
       xi = xi,
       phi = phi,
       D = D,
+      n_variables = length(mean_funs),
       type = ifelse(response, "response", "covariate")
-      )
+      ), class = "simMFPCA")
     )
+}
+
+#' Return Orthogonalized Population Quantities for Non-orthogonal Data Rotated by a matrix B
+#'
+#' @param Y A simMFPCA object
+#' @param B A matrix to rotate the data
+#' @param t A vector of time points
+#' @param sigma A scalar for the noise
+#'
+#' @return A list of (orthogonalized) population covariance, eigenfunctions, eigenscores, and B
+#' @export
+#'
+populationEigen = function(Y, B, t, sigma){
+  stopifnot(class(Y) == "simMFPCA")
+  stopifnot(is.matrix(B))
+  stopifnot(Y$type == "response")
+  stopifnot(all(dim(B) == c(ncol(Y$xi), ncol(Y$xi))))
+  stopifnot(length(t) == nrow(Y$phi)/Y$n_variables)
+
+  pop_covY = Y$phi %*% t(B) %*% t(Y$D) %*% Y$D %*% B %*% t(Y$phi) + diag(rep(sigma^2, Y$n_variables*length(t)))
+  pop_phiY = (eigen(pop_covY)$vectors[, 1:3]*sqrt(2*length(t))) %>% as.data.frame()
+  pop_xiY = Y$X %*% (eigen(pop_covY)$vectors[, 1:3])/sqrt(Y$n_variables*length(t))
+  pop_B = B %*% t(Y$phi) %*% (eigen(pop_covY)$vectors[, 1:3])/sqrt(Y$n_variables*length(t))
+
+  return(
+    list(
+      pop_covY = pop_covY,
+      pop_phiY = pop_phiY,
+      pop_xiY = pop_xiY,
+      pop_B = pop_B
+    )
+  )
 }
