b5c1f66f187ab0c03e2f573d9b97ffc7ca91318f
bpark67 <bpark67@uw.edu>
2024-11-24 23:25:48 -0800

    debugging CV

--- R/cvfunOnFun.R
+++ R/cvfunOnFun.R
@@ -22,13 +22,15 @@
 #' @param response An object of class irregMFPCA corresponding to the response
 #' @param actual A data frame with the actual values of the response
 #' @param folds Number of folds for cross-validation. Default is 10
+#' @param metric The metric to use for cross-validation. Default is "MSE"
 #'
 #' @return A matrix of mean squared errors for each combination of number of eigenfunctions
 #' @export
 #'
-cvfunOnFun = function(seed, predictor, response, actual, folds = 10){
+cvfunOnFun = function(seed, predictor, response, actual, folds = 10, metric = "MSE"){
   stopifnot(class(predictor) == "irregMFPCA")
   stopifnot(class(response) == "irregMFPCA")
+  stopifnot(metric %in% c("MSE", "R2"))
   set.seed(seed)
 
   # Get Length of time points
@@ -52,7 +54,6 @@ cvfunOnFun = function(seed, predictor, response, actual, folds = 10){
       PhiY = response$stacked_phi[, 1:j, drop = F]
       kfoldMSEs = rep(NA, folds)
 
-
       # Loop Through Folds
       # LOO
       for(k in 1:folds){
@@ -60,9 +61,12 @@ cvfunOnFun = function(seed, predictor, response, actual, folds = 10){
 
         # First do multivariate multiple regression to
         # Regress Eigenscores in TP2 on Eigenscores in TP1
-        B = stats::lm(Y[fld != k, ] ~ -1 + X[fld != k, ]) %>% stats::coef()
+        mod = stats::lm(Y[fld != k, ] ~ -1 + X[fld != k, ])
 
-        # Find estimates of eigenscores in TP2
+        # Extract coefficients
+        B = mod %>% stats::coef()
+
+        # Get fitted values
         Yhat = X[fld == k, , drop = F] %*% B
 
         # Find estimates of original NW and WR data
@@ -74,29 +78,69 @@ cvfunOnFun = function(seed, predictor, response, actual, folds = 10){
         colnames(orighat) = fld_case
         orighat$t = rep(seq(0, 1, length.out = time_points_y), nvars)
 
-        # Loop over all cases
-        MSEs = rep(NA, sum(fld == k))
+        if(metric == "R2"){
+          # ORIGINAL IDEA
+          # THIS PENALIZES HAVING TOO MANY PREDICTORS
+          # SST = sum((Y[fld == k, , drop = F] - colMeans(Y[fld == k, , drop = F]))^2)
+          # SSR = sum((Y[fld == k, , drop = F] - Yhat)^2)
 
-        for (case in fld_case){
-          # Find closest function values from our estimated functions
-          closest = .closest_t(actual$Time[[case]], seq(0, 1, length.out = time_points_y))
-          increment = rep(0:(nvars-1) * time_points_y, each = length(closest))
+          # MEAN BASED APPROACH
+          # SST = apply((Y[fld == k, , drop = F] - colMeans(Y[fld == k, , drop = F]))^2,
+          #             2,
+          #             sum) %>% mean()
+          # SSR = apply((Y[fld == k, , drop = F] - Yhat)^2,
+          #             2,
+          #             sum) %>% mean()
+          #
+          # val = 1 - SSR/SST
 
-          final = rep(closest, nvars) + increment
-          # Pull out corresponding estimates of the original
-          # These come from demeaned values; Is the demeaning done per case?
-          # Or is it done by the global mean?
-          orighat_case = orighat[final, which(case == fld_case)]
-          orig_case = actual[case, 3:(3+nvars-1)] %>% unlist() %>% unname()
+          R2s = rep(NA, sum(fld == k))
 
-          # Find Mean sum of squares error for this case
-          MSEs[which(case == fld_case)] = mean((orig_case-orighat_case)^2)
-          if(is.nan(mean((orig_case-orighat_case)^2))){
-            print(orig_case)
-            print(orighat_case)
+          for (case in fld_case){
+            # Find closest function values from our estimated functions
+            closest = .closest_t(actual$Time[[case]], seq(0, 1, length.out = time_points_y))
+            increment = rep(0:(nvars-1) * time_points_y, each = length(closest))
+
+            final = rep(closest, nvars) + increment
+
+            orighat_case = orighat[final, which(case == fld_case)]
+            orig_case = actual[case, 3:(3+nvars-1)] %>% unlist() %>% unname()
+
+            # The mean function is a 0 function.
+            SST = sum(orig_case^2)
+            SSR = sum((orig_case-orighat_case)^2)
+
+            R2s[which(case == fld_case)] = 1 - SSR/SST
+
+            val = mean(R2s)
+          }
+
+        }else if(metric == "MSE"){
+          # Loop over all cases
+          MSEs = rep(NA, sum(fld == k))
+
+          for (case in fld_case){
+            # Find closest function values from our estimated functions
+            closest = .closest_t(actual$Time[[case]], seq(0, 1, length.out = time_points_y))
+            increment = rep(0:(nvars-1) * time_points_y, each = length(closest))
+
+            final = rep(closest, nvars) + increment
+            # Pull out corresponding estimates of the original
+            # These come from demeaned values; Is the demeaning done per case?
+            # Or is it done by the global mean?
+            orighat_case = orighat[final, which(case == fld_case)]
+            orig_case = actual[case, 3:(3+nvars-1)] %>% unlist() %>% unname()
+
+            # Find Mean sum of squares error for this case
+            MSEs[which(case == fld_case)] = mean((orig_case-orighat_case)^2)
+            if(is.nan(mean((orig_case-orighat_case)^2))){
+              print(orig_case)
+              print(orighat_case)
+            }
+            val = mean(MSEs)
           }
         }
-        kfoldMSEs[k] = mean(MSEs)
+        kfoldMSEs[k] = val
       }
       fmatrix[i, j] = mean(kfoldMSEs)
     }
